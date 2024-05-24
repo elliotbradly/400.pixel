a682edc974249edfcb19fa291916d3b503d7a76f
Brad Henderson <elliotbradly@gmail.com>
2024-05-24 10:51:53 -0400

    can toss out events now

--- src/components/card.create-orb-type.vue
+++ src/components/card.create-orb-type.vue
@@ -1,23 +1,25 @@
 <template>
+  <q-card class="my-card bg-secondary text-white">
+    <q-card-section>
+      <div class="text-h6">Color Changing Pixels</div>
+      <div class="text-subtitle2">Covert one color scheme to another</div>
+    </q-card-section>
 
- <q-card class="my-card bg-secondary text-white">
-      <q-card-section>
-        <div class="text-h6">Color Changing Pixels</div>
-        <div class="text-subtitle2">Covert one color scheme to another</div>
-      </q-card-section>
+    <q-separator dark />
 
-      <q-separator dark />
-
-      <q-card-actions>
-        <q-btn flat>Change</q-btn>
-
-      </q-card-actions>
-    </q-card>
-
-
+    <q-card-actions>
+      <q-btn flat @click="$emit('title-event',  dex )">Change</q-btn>
+    </q-card-actions>
+  </q-card>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue'
+
+
+defineProps<{
+  dex?: number
+
+}>()
 
 </script>
--- src/pages/000.index.vue
+++ src/pages/000.index.vue
@@ -1,30 +1,19 @@
 <template>
   <div class="q-pa-md">
     <div class="q-gutter-md">
-      <q-carousel
-        v-model="slide"
-        transition-prev="scale"
-        transition-next="scale"
-        swipeable
-        animated
-        control-color="white"
-        navigation
-        padding
-        arrows
-        height="300px"
-        class="bg-primary text-white shadow-1 rounded-borders"
-      >
+      <q-carousel v-model="slide" transition-prev="scale" transition-next="scale" swipeable animated control-color="white"
+        navigation padding arrows height="300px" class="bg-primary text-white shadow-1 rounded-borders">
         <q-carousel-slide name="style" class="column no-wrap flex-center">
-        <OrbTypeCard></OrbTypeCard>
+          <OrbTypeCard @title-event="createAction" dex="1"></OrbTypeCard>
         </q-carousel-slide>
         <q-carousel-slide name="tv" class="column no-wrap flex-center">
-          <OrbTypeCard></OrbTypeCard>
+          <OrbTypeCard @title-event="createAction" dex="2"></OrbTypeCard>
         </q-carousel-slide>
         <q-carousel-slide name="layers" class="column no-wrap flex-center">
-          <OrbTypeCard></OrbTypeCard>
+          <OrbTypeCard @title-event="createAction" dex="3"></OrbTypeCard>
         </q-carousel-slide>
         <q-carousel-slide name="map" class="column no-wrap flex-center">
-          <OrbTypeCard></OrbTypeCard>
+          <OrbTypeCard @title-event="createAction" dex="4"></OrbTypeCard>
         </q-carousel-slide>
       </q-carousel>
 
@@ -44,8 +33,8 @@ import useFileList from '../compositions/file-list'
 const { files, addFiles, removeFile } = useFileList()
 
 function onInputChange(e) {
-	addFiles(e.target.files)
-	e.target.value = null // reset so that selecting the same file again will still cause it to fire this change
+  addFiles(e.target.files)
+  e.target.value = null // reset so that selecting the same file again will still cause it to fire this change
 }
 
 import createUploader from '../compositions/file-uploader'
@@ -53,8 +42,30 @@ const { uploadFiles } = createUploader('YOUR URL HERE')
 
 const bus = inject('bus')
 
+var createAction = (dex) => {
+
+  switch (dex) {
+
+    case 1:
+
+      break
+
+
+    case 2:
+
+      break
+
+    case 3:
+
+      break
+
+
+  }
+
+}
+
 var slide = ref('style');
-var autoplay =  ref(false)
+var autoplay = ref(false)
 var lorem = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
 
 bus.on('RENDER', (arg1, arg2, arg3) => {
@@ -62,16 +73,16 @@ bus.on('RENDER', (arg1, arg2, arg3) => {
 })
 
 onMounted(async () => {
-//  mount('on')
+  //  mount('on')
   //mountControl('on')
 })
 
 onUpdated(async () => {
-//  update('on')
+  //  update('on')
   //render()
 })
 
-onUnmounted(async () => {})
+onUnmounted(async () => { })
 
 </script>
 
